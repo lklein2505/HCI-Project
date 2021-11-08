5c2291839760d53ce7d41a5168fc6d45a8313b4f
Luka Klein <lklein00@fesb.hr>
2021-11-08 18:49:42 +0100

    initial commit - vjezba4

--- modules/header/header.js
+++ modules/header/header.js
@@ -1,41 +1,39 @@
-import React from 'react';
-import styles from './header.module.css';
 import Image from 'next/image';
 
 import LogoImg from '../../assets/logo.png';
 import HeroImg from '../../assets/hero.png';
-import Navbar from '../../components/navbar/navbar';
+import NavBar from '../../components/navbar';
 
 const Header = () => {
     return (
-        <header className="h-4/6 relative flex justify-center items-center">
-            <div className="z--1">
-                <Image
-                    src={HeroImg}
-                    layout="fill"
-                    objectFit="cover"
-                    alt="Hero image"
-                />
-            </div>
-            <main className="my-0 mx-auto flex flex-col min-w-sm max-w-6xl w-full">
-                <section className="flex justify-between items-center">
+        <header className="h-80 relative flex items-center">
+            <Image
+                src={HeroImg}
+                className="z-0"
+                layout="fill"
+                objectFit="cover"
+                placeholder="blur"
+                alt="Hero image"
+            />
+            <main className="max-w-4xl mx-auto flex-grow flex flex-col">
+                <div className="z-10 flex justify-between items-center mb-16">
                     <Image
                         src={LogoImg}
                         layout="fixed"
-                        width={150}
-                        height={150}
+                        width={50}
+                        height={50}
                         alt="Design logo"
                     />
-                    <Navbar selected="Home" />
-                </section>
-                <section className="mt-14">
-                    <h1 className="h-20 m-0 mb-1 font-bold text-7xl text-hci-lila">
+                    <NavBar />
+                </div>
+                <div className="z-10">
+                    <h1 className="text-5xl font-roboto-condensed font-bold mb-2 text-hci-lila">
                         Design Matters
                     </h1>
-                    <h3 className="h-14 m-0 font-normal text-5xl text-hci-lila opacity-70">
+                    <h3 className="text-3xl font-roboto-condensed font-light text-hci-lila text-opacity-60">
                         Entrust us with your digital appearance
                     </h3>
-                </section>
+                </div>
             </main>
         </header>
     );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,28 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {
-        colors: {
-            'hci-lila': '#8034AD',
+    // mode: 'jit',
+    // purge: [
+    //     './pages/**/*.{js,ts,jsx,tsx}',
+    //     './components/**/*.{js,ts,jsx,tsx}',
+    //     './modules/**/*.{js,ts,jsx,tsx}',
+    // ],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            fontFamily: {
+                'roboto-condensed': ['"Roboto Condensed"', 'sans-serif'],
+            },
+
+            colors: {
+                'hci-lila': {
+                    light: '#E3CEF0',
+                    DEFAULT: '#8034AD',
+                    dark: '#612D81',
+                },
+            },
         },
     },
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
